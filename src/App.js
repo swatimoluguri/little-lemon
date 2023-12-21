9a820215932b41f05a0802b018b742b1237a9107
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-12-21 20:28:51 +0530

    added styling to nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from "./components/Header";
-import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Footer from './components/Footer';
 
@@ -12,8 +11,6 @@ function App() {
     <meta name="og:description" content=""/>
     <meta name="og:image" content=""/>
     <Header>
-        <Nav>
-        </Nav>
     </Header>
     <Main>
     </Main>
