aa5268ceb3d82f9ca280b6970b20d63e23b376f1
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-12-22 19:57:51 +0530

    responsive nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,30 @@
+import { Component } from "react";
 import SmallLogo from "../assets/logo-s.jpg";
 import './Nav.css';
 
-function Nav(){
-    return (
-        <nav>
-            <div>
-                <img src={SmallLogo}  alt="Little Lemon Restaurant Logo" />
-            </div>
-            <div>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/">Book a Table</a></li>
-                    <li><a href="/">Contact Us</a></li>
-                </ul>
-            </div>
-
-        </nav>
-    );
+class Nav extends Component{
+    state={clicked:false};
+    handleClick=()=>{
+        this.setState({clicked:!this.state.clicked})
+    }
+    render(){
+        return (
+            <nav >
+                <div>
+                    <img src={SmallLogo}  alt="Little Lemon Restaurant Logo" />
+                </div>
+                <div>
+                    <ul id="navbar" className={this.state.clicked?"#navbar active":"#navbar"}>
+                        <li><a href="/" title="Home">Home</a></li>
+                        <li><a href="/" title="Book a Table">Book a Table</a></li>
+                        <li><a href="/" title="Book a Table">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div id="mobile" onClick={this.handleClick}>
+                    <i id="bar" className={this.state.clicked?'fas fa-times':'fas fa-bars'}></i>
+                </div>
+            </nav>
+        );
+    }
 }
 export default Nav;
