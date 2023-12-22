5085c9c319ac4a8049edad5cd2994ce808c9a483
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-12-22 21:09:42 +0530

    added hero

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,19 @@
+import "./Main.css";
+import HeroImg from "../assets/hero.jpg";
+
 function Main(){
-    return <></>;
+    return (
+        <section class="hero">
+            <div class="leftHero">
+                <span>Little Lemon</span>
+                <p>Chicago</p>
+                <h3>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</h3>
+            <button>Reserve a Table</button>
+            </div>
+            <div class="rightHero">
+                <img src={HeroImg} alt=""/>
+            </div>
+        </section>
+    );
 }
 export default Main;
