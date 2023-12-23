b33358ee3fba0cd49dc0eea2e1b6de25e0bde3ab
swatimoluguri <149689000+swatimoluguri@users.noreply.github.com>
2023-12-23 15:59:31 +0530

    added description and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,41 @@
-function Footer(){
-    return <></>;
+import "./Footer.css";
+import SmallLogo from "../assets/logo-s.jpg";
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div id='leftFooter'>
+                    <img src={SmallLogo} alt="Little Lemon Restaurant Logo" />
+                </div>
+                <div id='rightFooter'>
+                    <div><ul>
+                        <h2>Navigation</h2>
+                        <li><a title="Home">Home</a></li>
+                        <li><a title="Menu">Menu</a></li>
+                        <li><a title="Book a Table">Book a Table</a></li>
+                        <li><a title="Order Online">Order Online</a></li>
+                        <li><a title="About">About</a></li>
+                        <li><a title="Login/Signin">Login/Signin</a></li>
+                    </ul></div>
+                    <div>
+                    <ul>
+                        <h2>Contact</h2>
+                        <li><a title="Phone">Phone</a></li>
+                        <li><a title="Email">Email</a></li>
+                        <li><a title="Address">Address</a></li>
+                    </ul>
+                    </div>
+                    <div>
+                    <ul>
+                        <h2>Social Media</h2>
+                        <li><a title="Instagram">Instagram</a></li>
+                        <li><a title="Facebook">Facebook</a></li>
+                        <li><a title="Twitter">Twitter</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </footer></>
+    );
 }
 export default Footer;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import "./Main.css";
+import Kitchen from "../assets/kitchen.jpg";
 import HeroImg from "../assets/hero.jpg";
+import Restaurant from "../assets/restaurant.jpg";
 import Card from './Card';
 import Review from './Review';
 
@@ -24,6 +26,17 @@ function Main() {
             <section id="testimonials">
                 <h1>Testimonials</h1>
                 <Review />
+            </section>
+            <section class="description">
+                <div class="leftdescrition">
+                    <h1>Little Lemon</h1>
+                    <h3>Chicago</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at ex leo. Maecenas enim sem, laoreet at nulla ac, luctus scelerisque massa. Praesent ut molestie nisi. Aliquam arcu lorem, auctor condimentum blandit id, lobortis in nisi. Ut diam justo, euismod in accumsan id, vehicula sit amet tellus. Pellentesque porttitor elit lacus, vitae lacinia magna ultricies quis. Etiam vitae tellus et mi hendrerit consequat. In dictum ligula in elit euismod malesuada. Curabitur varius augue id tempus ultricies. Mauris suscipit porta odio, et pretium nibh scelerisque ut.</p>
+                </div>
+                <div class="rightdescrition">
+                    <img id="kitchen" src={Kitchen} alt="" />
+                    <img id="restaurant" src={Restaurant} alt="" />
+                </div>
             </section>
         </>
     );
